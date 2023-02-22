0d70690f20056012e2f1c3f25091c7dd43f9b9f9
FaceOfBo0 <70140104+FaceOfBo0@users.noreply.github.com>
2023-02-22 23:32:23 +0100

    zrdz

---
+++ src/main/java/ODSFileWriter.java
@@ -0,0 +1,34 @@
+import com.github.jferard.fastods.*;
+import com.github.jferard.fastods.tool.FastOds;
+
+import java.io.File;
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.net.URL;
+
+public class ODSFileWriter {
+
+    public static void main(String[] args) throws IOException {
+        System.out.println("Hello world");
+        testRun();
+    };
+
+    public static void testRun() throws IOException {
+        OdsFactory factory;
+        factory = OdsFactory.create();
+        AnonymousOdsFileWriter odsWriter = factory.createWriter();
+        OdsDocument document = odsWriter.document();
+        Table table = document.addTable("Wochenplan");
+        TableRowImpl row = table.getRow(0);
+        TableCell cell = row.getOrCreateCell(0);
+        cell.setStringValue("Halle Wellt!");
+        URL url = new URL("https://www.uni-frankfurt.de");
+
+        cell.setText(Text.builder().par().span("Before ").link("link Text", url).build());
+        odsWriter.saveAs(new File("src/main/resources/test.ods"));
+
+
+
+
+    }
+}
