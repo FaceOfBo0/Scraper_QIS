eff3451ac4d2e15841af3cfc7f00b33a5420d881
FaceOfBo0 <70140104+FaceOfBo0@users.noreply.github.com>
2023-02-24 13:32:21 +0100

    updated Lecture & Lecture_Impl

--- src/main/java/data/Lecture_Impl.java
+++ src/main/java/data/Lecture_Impl.java
@@ -1,66 +1,80 @@
 package data;
 import org.jsoup.nodes.Document;
+
+import java.util.ArrayList;
 import java.util.List;
 
 public class Lecture_Impl implements Lecture{
 
+    private String day;
+    private List<String> lecturersList;
+    private String time;
+    private String name;
+    private List<String> modulesList;
+    private Document document;
+
+    Lecture_Impl(){
+        this.lecturersList = new ArrayList<>(0);
+        this.day = "";
+    }
+
     @Override
     public void setDay(String day) {
-
+        this.day = day;
     }
 
     @Override
     public String getDay() {
-        return null;
+        return this.day;
     }
 
     @Override
     public void setLecturer(String lecturer) {
-
+        this.lecturersList.add(lecturer);
     }
 
     @Override
     public List<String> getLecturers() {
-        return null;
+        return this.lecturersList;
     }
 
     @Override
     public void setTime(String time) {
-
+        this.time = time;
     }
 
     @Override
     public String getTime() {
-        return null;
+        return this.time;
     }
 
     @Override
     public void setName(String name) {
-
+        this.name = name;
     }
 
     @Override
     public String getName() {
-        return null;
+        return this.name;
     }
 
     @Override
     public void setModulesList(List<String> modules) {
-
+        this.modulesList = modules;
     }
 
     @Override
     public List<String> getModulesList() {
-        return null;
+        return this.modulesList;
     }
 
     @Override
     public void setDocument(Document doc) {
-
+        this.document = doc;
     }
 
     @Override
     public Document getDocument() {
-        return null;
+        return this.document;
     }
 }
