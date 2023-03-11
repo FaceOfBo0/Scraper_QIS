ea9f1ffc2f0c35a8a0254b139c2817f818c10eb3
Tim <koenigt@stud.uni-frankfurt.de>
2023-03-11 14:54:09 +0100

    updated ODSFileWriter

--- src/main/java/helper/ODSFileWriter.java
+++ src/main/java/helper/ODSFileWriter.java
@@ -6,27 +6,40 @@ import java.io.File;
 import java.io.IOException;
 
 public class ODSFileWriter {
+OdsDocument document;
+AnonymousOdsFileWriter fileWriter;
 
-    public void ODSFileWriter(){
-
-    }
-    public void createTable() throws IOException {
+    public ODSFileWriter(){
         OdsFactory factory;
         factory = OdsFactory.create();
-        AnonymousOdsFileWriter odsWriter = factory.createWriter();
-        OdsDocument document = odsWriter.document();
-        Table table = document.addTable("Wochenplan");
-        TableCellWalker cellWalker = table.getWalker();
-        for (int r = 0; r < 10;r++){
-            for (int c = 0; c<9; c++){
-                cellWalker.setStringValue((char) (c + 'A') + String.valueOf(r + 1));
-                cellWalker.next();
+        this.fileWriter = factory.createWriter();
+        this.document = this.fileWriter.document();
+    }
+
+    public void createTable() {
+        Table table = null;
+        try {
+            table = this.document.addTable("Wochenplan");
+            TableCellWalker cellWalker = table.getWalker();
+            for (int r = 0; r < 10;r++){
+                for (int c = 0; c<9; c++){
+                    cellWalker.setStringValue((char) (c + 'A') + String.valueOf(r + 1));
+                    cellWalker.next();
+                }
+                cellWalker.nextRow();
             }
-            cellWalker.nextRow();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
         }
+
 //        URL url = new URL("https://www.uni-frankfurt.de");
 //        cell.setText(Text.builder().par().span("Before ").link("link Text", url).build());
-        odsWriter.saveAs(new File("src/main/resources/test.ods"));
-
+    }
+    public void saveDocAsODS(String fileName){
+        try {
+            this.fileWriter.saveAs(new File ("src/main/resources/"+fileName));
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
