f7c391601b530768b14ea5b581a2541a552ce692
Tim <koenigt@stud.uni-frankfurt.de>
2023-07-02 10:28:25 +0200

    webserver update

--- src/main/java/webserver/WebServer.java
+++ src/main/java/webserver/WebServer.java
@@ -1,11 +1,30 @@
 package webserver;
 
 import data.LectureFactory;
+import freemarker.template.Configuration;
+import spark.Spark;
+
+import java.io.File;
+import java.io.IOException;
 
 public class WebServer {
     private LectureFactory factory;
+    private static final Configuration config = new Configuration(Configuration.VERSION_2_3_26);
 
-    public WebServer(){
+    public WebServer(int pPort){
+        Spark.port(pPort);
+        Spark.staticFileLocation("templates/javascript");
+        Spark.externalStaticFileLocation("templates/javascript");
+        Spark.staticFiles.location("templates/javascript");
+        Spark.staticFiles.externalLocation("templates/javascript");
+        try {
+            config.setDirectoryForTemplateLoading(new File("templates/"));
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public void runRoutes(){
 
     }
 }
