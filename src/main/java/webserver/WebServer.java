8cf9425ebdb4be4fc44ccfa13fd2a2864aba60f5
FaceOfBo0 <70140104+FaceOfBo0@users.noreply.github.com>
2023-07-03 18:46:09 +0200

    changed templates folder again

--- src/main/java/webserver/WebServer.java
+++ src/main/java/webserver/WebServer.java
@@ -6,28 +6,22 @@ import spark.ModelAndView;
 import spark.Spark;
 import spark.template.freemarker.FreeMarkerEngine;
 
-import java.io.File;
-import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
 
 public class WebServer {
     private LectureFactory factory;
-    private static final Configuration config = new Configuration(Configuration.VERSION_2_3_26);
+    private final Configuration config = new Configuration(Configuration.VERSION_2_3_26);
 
     public WebServer(int pPort){
         Spark.port(pPort);
-        try {
-            config.setDirectoryForTemplateLoading(new File("./"));
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
+        this.config.setClassLoaderForTemplateLoading(ClassLoader.getSystemClassLoader(), "templates");
     }
 
     public void runRoutes(){
     Spark.get("/","text/html", (req, res) -> {
         Map<String, Object> attributes = new HashMap<>(0);
         return new ModelAndView(attributes, "root.ftl");
-    }, new FreeMarkerEngine(config));
+    }, new FreeMarkerEngine(this.config));
     }
 }
